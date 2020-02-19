f134c720b460c1fa7fdd03ef5332a27d1e36f0d7
Matt Difford <matt@mrsavvy.co.uk>
2020-02-19 10:30:22 +0000

    Turns out I did need some JS...

--- js/bundle.js
+++ js/bundle.js
@@ -1,7 +1,4 @@
 ---
 ---
 
-{% include_relative  src/modernizr.min.js %}
-{% include_relative  src/script.js %}
 {% include_relative  src/lazysizes.min.js %}
-{% include_relative  src/parsley.min.js %}
